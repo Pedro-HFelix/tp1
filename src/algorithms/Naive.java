a51ae28b91b6d9c32c85411cda04ce6365354a50
MatheusSCxr <matheus.coxir@sga.pucminas.br>
2025-10-04 18:45:17 -0300

    Add: Main simples para testes no Naive

--- src/algorithms/Naive.java
+++ src/algorithms/Naive.java
@@ -22,4 +22,47 @@ public class Naive {
         }
         return bridge;
     }
+
+    public static void main(String args[]) {
+        // Cria os grafos dos exemplos
+        System.out.println("Aplicando o Naive no primeiro grafo (Bridge1):");
+
+        Graph g1 = new Graph(5);
+        g1.addEdge(2, 1);
+        g1.addEdge(1, 3);
+        g1.addEdge(3, 2);
+        g1.addEdge(1, 4);
+        g1.addEdge(4, 5);
+
+        for (int[] pontes : foundBridge(g1)){
+            System.out.println("Aresta-ponte encontrada: (" + pontes[0] + ", " + pontes[1] + ")");
+        }
+        System.out.println();
+
+        System.out.println("Aplicando o Naive no segundo grafo (Bridge2):");
+        Graph g2 = new Graph(7);
+        g2.addEdge(1, 2);
+        g2.addEdge(2, 3);
+        g2.addEdge(3, 1);
+        g2.addEdge(2, 4);
+        g2.addEdge(2, 5);
+        g2.addEdge(2, 7);
+        g2.addEdge(4, 6);
+        g2.addEdge(5, 6);
+
+        for (int[] pontes : foundBridge(g2)){
+            System.out.println("Aresta-ponte encontrada: (" + pontes[0] + ", " + pontes[1] + ")");
+        }
+        System.out.println();
+
+        System.out.println("Aplicando o Naive no terceiro grafo (Bridge3):");
+        Graph g3 = new Graph(4);
+        g3.addEdge(1, 2);
+        g3.addEdge(2, 3);
+        g3.addEdge(3, 4);
+
+        for (int[] pontes : foundBridge(g3)){
+            System.out.println("Aresta-ponte encontrada: (" + pontes[0] + ", " + pontes[1] + ")");
+        }
+    }
 }
